caebce40fc50be7b1bba2debe02404269a6e7bd7
juangarcia16 <juangarciaoliva163@gmail.com>
2022-01-14 10:15:34 +0100

    Entrega del control

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,16 +2,42 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "feedings")
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+public class Feeding  extends BaseEntity{
+	@NotNull
+	@Column(name = "start_date")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate startDate;
+    
+    @Column(name = "weeks_duration")
+    @NotNull
+    @Min(1)
     double weeksDuration;
-    Pet pet;   
+    
+    @ManyToOne
+    @NotNull
+    Pet pet; 
+
+    @ManyToOne
+    @JoinColumn(name = "feeding_type_id")
+    FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,19 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedingRepository extends CrudRepository<Feeding, Integer>{
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+    
+    @Query("SELECT f FROM FeedingType f")
+    List<FeedingType> findAllFeedingTypes();
+    
+    @Query("SELECT f.feedingType FROM Feeding f WHERE f.feedingType.name LIKE :name%")
+    FeedingType getFeedingType(String name);
+    
     Optional<Feeding> findById(int id);
+    
     Feeding save(Feeding p);
 }
